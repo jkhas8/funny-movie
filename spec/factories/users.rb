f54e0b0de40e70f62eee490a7b485c1afce12abb
jkhas <nguyenthai2811@gmail.com>
2020-09-12 15:37:32 +0700

    feat(model): add user model

---
+++ spec/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :user do
+    email "email_test@test.test"
+    password "12345678"
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,47 @@
+require "rails_helper"
+
+RSpec.describe User, type: :model do
+  let (:user) { build :user }
+
+  subject { user }
+
+  context "when email is blank" do
+    before { subject.email = " " }
+    it { is_expected.not_to be_valid }
+  end
+
+  context "when email is empty" do
+    before { subject.email = "" }
+    it { is_expected.not_to be_valid }
+  end
+
+  context "when email is nil" do
+    before { subject.email = nil }
+    it { is_expected.not_to be_valid }
+  end
+
+  context "when email is too long" do
+    before { subject.email = "a" * 255 + "@test.com" }
+    it { is_expected.not_to be_valid }
+  end
+
+  context "when email is invalid" do
+    before { subject.email = "wrong format" }
+    it { is_expected.not_to be_valid }
+  end
+
+  context "when password is blank" do
+    before { subject.password = " " }
+    it { is_expected.not_to be_valid }
+  end
+
+  context "when password is nil" do
+    before { subject.password = nil }
+    it { is_expected.not_to be_valid }
+  end
+
+  context "when password is too sort" do
+    before { subject.password = "a" * 7 }
+    it { is_expected.not_to be_valid }
+  end
+end
